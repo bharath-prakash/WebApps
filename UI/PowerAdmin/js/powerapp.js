2e9c4c93a212d439d4783051949399bdd2a9bcb5
Bharath Prakash <bharathbmsce@gmail.com>
2014-02-03 17:36:41 +0530

    added calender to dashboard

--- UI/PowerAdmin/js/powerapp.js
+++ UI/PowerAdmin/js/powerapp.js
@@ -132,11 +132,91 @@ $('#left-panel').css('min-height',bodyHeight+'px');
 			$('#hide-menu').click(function(event){
 			$('body').toggleClass('hidden-menu');
 			
-			})
+			});
 
+
+
+var date = new Date();
+		var d = date.getDate();
+		var m = date.getMonth();
+		var y = date.getFullYear();
+		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'title'				
+			},
+			editable: true,
+			events: [
+				{
+					title: 'All Day Meeting',
+					start: new Date(y, m, 1)
+				},
+				{
+					title: 'Customer requirement workshop',
+					start: new Date(y, m, d-5),
+					end: new Date(y, m, d-2)
+				},
+				{
+					id: 999,
+					title: 'Weekly Meeting',
+					start: new Date(y, m, d-3, 16, 0),
+					allDay: false
+				},
+				{
+					id: 999,
+					title: 'Weekly Meeting',
+					start: new Date(y, m, d+4, 16, 0),
+					allDay: false
+				},
+				{
+					title: 'Meeting',
+					start: new Date(y, m, d, 10, 30),
+					allDay: false
+				},
+				{
+					title: 'Lunch',
+					start: new Date(y, m, d, 12, 0),
+					end: new Date(y, m, d, 14, 0),
+					allDay: false
+				},
+				{
+					title: 'Birthday Party',
+					start: new Date(y, m, d+1, 19, 0),
+					end: new Date(y, m, d+1, 22, 30),
+					allDay: false
+				},
+				{
+					title: 'Click for Google',
+					start: new Date(y, m, 28),
+					end: new Date(y, m, 29),
+					url: 'http://google.com/'
+				}
+			]
+		});
+
+	$('.power-widget .fc-header .fc-header-center').hide();
+			$('.power-widget .fc-header .fc-header-right').hide();
+
+$('#calendar-buttons #btn-prev').click(function () {
+	    $('.fc-button-prev').click();
+	    return false;
+	});
+	
+	// calendar next
+	$('#calendar-buttons #btn-next').click(function () {
+	    $('.fc-button-next').click();
+	    return false;
+	});
 	
 	});
 	
+
+	$(document).on("click",".power-widget .power-widget-header .controls .fa-expand",function(event){
+		$(this).parent().parent().parent().toggleClass('fullscreen-mode');
+
+	});
+
+
 	$(document).on("click","#left-panel ul li a",function(event){
 		event.preventDefault();
 		if($(this).find(".collapse-sign em").hasClass("fa-caret-down")){
